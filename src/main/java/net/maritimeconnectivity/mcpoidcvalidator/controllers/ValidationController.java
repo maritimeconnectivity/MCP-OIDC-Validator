ec77f49666ef7922c770d9c5795711556d8a6af7
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2021-10-13 16:56:46 +0200

    A small simplification

--- src/main/java/net/maritimeconnectivity/mcpoidcvalidator/controllers/ValidationController.java
+++ src/main/java/net/maritimeconnectivity/mcpoidcvalidator/controllers/ValidationController.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +19,7 @@ public class ValidationController {
             value = "/validate",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<String> validateToken() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        JwtAuthenticationToken jwt = (JwtAuthenticationToken) auth;
+    public ResponseEntity<String> validateToken(JwtAuthenticationToken jwt) {
         String uid = (String) jwt.getTokenAttributes().get("uid");
         return new ResponseEntity<>("UID: " + uid, HttpStatus.OK);
     }
