d5b9ec08640eea2e87b6aedf9518303e2f4d0df4
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2021-10-20 17:06:37 +0200

    Some more progress

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,15 +1,38 @@
 const config = {
     authority: 'https://test-maritimeid.maritimeconnectivity.net/auth/realms/MCP',
     client_id: 'validator',
-    redirect_uri: 'http://localhost:8080/callback.html',
+    redirect_uri: window.location.origin + '/callback.html',
     response_type: 'code',
-    scope: 'openid'
+    scope: 'openid',
+    automaticSilentRenew: true
 };
 
-let userManager = new Oidc.UserManager(config);
+const userManager = new Oidc.UserManager(config);
 
-let loginButton = document.getElementById("login");
-loginButton.addEventListener('click', () => userManager.signinRedirect());
+const loginButton = document.getElementById("login");
+const logOutButton = document.getElementById("logout");
+const validateButton = document.getElementById("validate");
 
-let logOutButton = document.getElementById("logout");
-logOutButton.addEventListener('click', () => userManager.signoutRedirect({post_logout_redirect_uri: window.location.href}));
+let user = await userManager.getUser();
+
+if (!user) {
+    loginButton.addEventListener('click', () => userManager.signinRedirect());
+    logOutButton.hidden = true;
+    validateButton.hidden = true;
+} else {
+    logOutButton.addEventListener('click', () => {
+        userManager.signoutRedirect({post_logout_redirect_uri: window.location.href});
+    });
+    loginButton.hidden = true;
+    validateButton.addEventListener('click', async () => {
+        console.log(user.profile);
+        let response = await fetch('/api/validate', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Authorization': 'Bearer ' + user.access_token
+            }
+        });
+        console.log(response);
+    });
+}
