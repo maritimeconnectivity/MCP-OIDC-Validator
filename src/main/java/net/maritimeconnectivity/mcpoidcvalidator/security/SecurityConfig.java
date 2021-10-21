c777dea3474e7d914775c6612969d699502eb899
Oliver Haagh <oliver@dmc.international>
2021-10-21 15:26:55 +0200

    Working SecurityConfig

--- src/main/java/net/maritimeconnectivity/mcpoidcvalidator/security/SecurityConfig.java
+++ src/main/java/net/maritimeconnectivity/mcpoidcvalidator/security/SecurityConfig.java
@@ -1,38 +1,19 @@
 package net.maritimeconnectivity.mcpoidcvalidator.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity webSecurity) throws Exception {
-        super.configure(webSecurity);
-        webSecurity
-                .ignoring()
-                .antMatchers(
-                        "/",
-                        "/js/*.js",
-                        "/*.html",
-                        "/favicon.ico"
-                );
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(
-                        "/",
-                        "/js/*.js",
-                        "/*.html",
-                        "/favicon.ico"
-                ).permitAll()
-                .anyRequest()
-                .authenticated();
+                .authorizeRequests(authz -> authz
+                        .antMatchers("/api/**").authenticated()
+                        .antMatchers("/**").permitAll())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
