b62dd38d22deea1fdf141998e6715f8f53d3d197
Oliver Haagh <oliver@dmc.international>
2021-10-22 15:39:20 +0200

    We can now parse the ID token and show its content in a table

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -13,6 +13,9 @@ const loginButton = document.getElementById("login");
 const logOutButton = document.getElementById("logout");
 const validateButton = document.getElementById("validate");
 
+const claims = ["uid", "flagstate", "callsign", "imo_number", "mmsi", "ais_type", "registered_port", "ship_mrn", "mrn",
+    "permissions", "subsidiary_mrn", "mms_url", "mms_url"];
+
 let user = await userManager.getUser();
 
 if (!user) {
@@ -26,16 +29,26 @@ if (!user) {
     loginButton.hidden = true;
     validateButton.addEventListener('click', async () => {
         user = await userManager.getUser();
-        let response = await fetch('/api/validate', {
+        const response = await fetch('/api/validate', {
             method: 'GET',
             mode: 'cors',
             headers: {
                 'Authorization': 'Bearer ' + user.access_token
             }
         });
-        const status = response.status;
-        if (status === 200) {
+        if (response.status === 200) {
             alert(await response.text());
         }
+        const idToken = user.id_token;
+        const idTokenSplit = idToken.split('.');
+        const id = JSON.parse(atob(idTokenSplit[1]));
+        claims.forEach(claim => {
+           const claimValue = id[claim];
+           if (claimValue) {
+               const tableRow = document.getElementById(claim);
+               tableRow.cells[1].textContent = claimValue;
+               tableRow.cells[2].textContent = "\u2705";
+           }
+        });
     });
 }
